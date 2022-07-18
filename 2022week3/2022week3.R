b9c0c7ce6445b1275491f7da1710d7bd45e85905
ThymeLy <38356458+ThymeLy@users.noreply.github.com>
2022-07-18 15:42:38 +0900

    Create 2022week3.R

---
+++ 2022week3/2022week3.R
@@ -0,0 +1,31 @@
+# load data and library
+tuesdata <- tidytuesdayR::tt_load('2022-01-18')
+chocolate <- tuesdata$chocolate
+
+library(tidyverse)
+library(networkD3)
+
+# retain only main columns
+choc_shortened <- chocolate[c("country_of_bean_origin", "cocoa_percent", "ingredients", "rating")]
+
+# round off rating 
+choc_shortened$rating <- round(choc_shortened$rating, 0)
+
+# calculate rating assoc with each country to give value for Sankey diagram, retaining only n > 10
+choc4sankey <- choc_shortened %>% group_by(country_of_bean_origin, rating) %>% count() %>% filter(n>10) %>% arrange(desc(rating))
+
+# Create nodes for source and target dataframe
+nodes <- data.frame(name=c(as.character(choc4sankey$rating), as.character(choc4sankey$country_of_bean_origin)) %>% unique())
+
+# Match source and target ID position
+choc4sankey$IDsource=match(choc4sankey$rating, nodes$name)-1 
+choc4sankey$IDtarget=match(choc4sankey$country_of_bean_origin, nodes$name)-1
+
+# plot Sankey diagram, iterations = 0 to position rating 4 at the top
+sankey <- sankeyNetwork(Links = choc4sankey, Nodes = nodes,
+              Source = "IDsource", Target = "IDtarget",
+              Value = "n", NodeID = "name", 
+              nodeWidth=40, fontSize=13, iterations = 0, width = 900, height = 580)
+
+# append plot title
+htmlwidgets::prependContent(sankey, htmltools::tags$h1("Which country produced the best chocolate?", style= "text-align:center"))
